0af8b5473f81bc688bdc5d2d8a0c2d5a2232a685
Vladyslav Sidikov <vlad.sidikov007@gmai.com>
2020-05-25 18:41:14 +0300

    created a funcional

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+import { googleTranslate } from "./utils/googleTranlate";
+
+const App = () => {
+  const localHistory = localStorage.history;
+  const [languageCodes, setLanguageCodes] = useState([]);
+  const [language, setLanguage] = useState("en");
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [history, setHistory] = useState(
+    localHistory ? JSON.parse(localHistory) : []
+  );
+
+  useEffect(() => {
+    googleTranslate.getSupportedLanguages("ru", function (err, languageCodes) {
+      getLanguageCodes(languageCodes);
+    });
+    const getLanguageCodes = (languageCodes) => {
+      setLanguageCodes(languageCodes);
+    };
+  }, []);
+
+  const changeHandler = () => {
+    const translating = (transQuestion) => {
+      if (question !== transQuestion) {
+        setAnswer(transQuestion);
+        setHistory((prevHistory) => {
+          const res = [...prevHistory, { question, transQuestion }];
+          localStorage.setItem("history", JSON.stringify(res));
+          return res;
+        });
+      }
+    };
+
+    googleTranslate.translate(question, language, function (err, translation) {
+      translating(translation.translatedText);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{}}>
+      <input
+        type="text"
+        value={question}
+        onChange={(event) => setQuestion(event.target.value)}
+        placeholder="Enter text"
+      />
+      <input type="text" value={answer} readOnly />
+
+      <select
+        value={language}
+        onChange={(event) => setLanguage(event.target.value)}
+      >
+        {languageCodes.map((lang) => (
+          <option key={lang.language} value={lang.language}>
+            {lang.name}
+          </option>
+        ))}
+      </select>
+      <button onClick={changeHandler}>lang</button>
+      <div className="history">
+        <ol>
+          {history.map((item, i) => (
+            <li key={i}>
+              {item.question} - {item.transQuestion}
+            </li>
+          ))}
+        </ol>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/utils/googleTranlate.js
@@ -0,0 +1,2 @@
+const apiKey = process.env.REACT_APP_GOOGLE_TRANSLATE_API_KEY;
+export const googleTranslate = require("google-translate")(apiKey);
